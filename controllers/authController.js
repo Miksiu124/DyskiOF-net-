ff1be88b347b7abf0457e990a407db1c16a44a8a
Miksiu124 <hardliferoot@gmail.com>
2025-08-07 14:28:51 +0200

    Add bcrypt password hashing

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,32 @@
-exports.register = (req, res) => {/* handle registration */};
-exports.login = (req, res) => {/* handle login */};
-exports.discordOAuth = (req, res) => {/* handle Discord OAuth */};
+const User = require('../models/User');
+
+exports.register = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = new User({ email, password });
+    await user.save();
+    res.status(201).json({ message: 'User registered' });
+  } catch (err) {
+    res.status(500).send('Server error');
+  }
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ msg: 'Invalid credentials' });
+
+    const isMatch = await user.comparePassword(password);
+    if (!isMatch) return res.status(400).json({ msg: 'Invalid credentials' });
+
+    res.json({ message: 'Logged in' });
+  } catch (err) {
+    res.status(500).send('Server error');
+  }
+};
+
+exports.discordOAuth = (req, res) => {
+  /* handle Discord OAuth */
+};
+
